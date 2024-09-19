7b52de83ff44133cb9c00551c38844d86c32c706
Qwerty123456As1234 <evlinges>
2024-09-19 15:40:22 +0300

    Додано мій перший Python скрипт

--- evel.py
+++ evel.py
@@ -1,24 +1,16 @@
-print ('ВІтаємо вас у калькуляторі Python')
-q1 = int (input('Ведіть число 1: '))
-q2 = int (input('Ведіть число 2: '))
+class Board:
+	def __init__(self, sizes: Iterable[int]):
+		val = self.get_empty_cell_value()
+		self.cells = ?
 
-v = int (input('Яку операцію ви хочете виконати? \n 1 Додавання \n 2 Віднімання \n 3 Ділення \n 4 Множення \n'))
+	def __getitem__(self, idx: Iterable[int]):
+		pass
 
-if v == 1:
-    r = q1 + q2
-    p = 'додавання'
-    t = p
-if v == 2:
-    r = q1 - q2
-    l = 'віднімання'
-    t = l
-if v == 3:
-    r = float(q1 / q2)
-    m = 'ділення'
-    t = m
-if v == 4:
-    r = q1 * q2
-    n = 'множення'
-    t = n
-print ('Результат ',t,' = ',r)
+	def __setitem__(self, idx: Iterable[int], val):
+		pass
+
+
+	@classmethod
+	def get_empty_cell_value(cls):
+		return None
     
